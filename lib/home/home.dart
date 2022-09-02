fa6446c39a977985a167969d46da84899d705265
Ashish <ashishpise111@gmail.com>
2022-09-02 20:04:31 +0530

    added auth service

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:lmr/dashboard/dashboard.dart';
+import 'package:lmr/services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DashboardScreen();
+    return StreamBuilder(stream: AuthService().userStream,
+    builder: ((context, snapshot) {
+       if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('loading');
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text('Error'),
+          );
+        } else if (snapshot.hasData) {
+          return const DashboardScreen();
+        } else {
+          return const Text('Login');
+        }
+    }));
   }
 }
---
+++ lib/services/auth.dart
@@ -0,0 +1,26 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService{
+  final userStream = FirebaseAuth.instance.authStateChanges();
+  final user = FirebaseAuth.instance.currentUser;
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  Future<void> googleLogin() async {
+    try {
+      final googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) return;
+      final googleAuth = await googleUser.authentication;
+      final authCredential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+      await FirebaseAuth.instance.signInWithCredential(authCredential);
+    } on FirebaseAuthException catch (e) {
+      log(e.message!);
+    }
+  }
+}
