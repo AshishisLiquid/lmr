c9ff4ca7ef345f4774ad9df71ab1829598a41cb1
Ashish <ashishpise111@gmail.com>
2022-09-02 20:18:04 +0530

    added auth guard and screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,7 @@
+import 'package:lmr/auth/login.dart';
 import 'package:lmr/home/home.dart';
 
 var appRoutes = {
   '/': (context) => const HomeScreen(),
+  '/login': (context) => const LoginScreen(),
 };
