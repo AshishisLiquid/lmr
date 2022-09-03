06e11f7d129c43f541ed0164e58cfbf9143b59ba
AshishPis3 <ashishpise@syntags.co>
2022-09-03 13:59:09 +0530

    Added register with email

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,9 +3,23 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class AuthService{
+class AuthService {
   final userStream = FirebaseAuth.instance.authStateChanges();
   final user = FirebaseAuth.instance.currentUser;
+
+  Future<void> signUpWithEmailandPassword(
+      String email, String password, String displayname) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        log('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        log('Wrong password provided for that user.');
+      }
+    }
+  }
 
   Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
